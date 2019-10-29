e1993489ea490fd87e0195708f29dabe30a925ad
yuexiasheng <1576163541@qq.com>
2019-10-29 23:47:06 +0800

    内容添加

--- src/z/Main.java
+++ src/z/Main.java
@@ -3,6 +3,10 @@ package z;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("xxxxxxx");
+        System.out.println("xxxxxxx");
+        System.out.println("xxxxxxx");
+        System.out.println("xxxxxxx");
 	// write your code here
     }
 }
