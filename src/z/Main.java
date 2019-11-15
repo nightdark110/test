8b1143fdda915192b325ca5b85c255d0d88c77bd
yuexiasheng <1576163541@qq.com>
2019-11-15 16:54:40 +0800

    冲突解决

--- src/z/Main.java
+++ src/z/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("xxxxxxx");
         System.out.println("xxxxxxx");
-        System.out.println("xxxxxxx");
-        System.out.println("xxxxxxx");
+        System.out.println("xxxxxxxdddddddddd");
 	// write your code here
     }
 }
